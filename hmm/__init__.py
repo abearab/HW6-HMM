ebb686a8d4bc9a2620cbc92ef9eedc7fc5a969cc
Abe Arab <abarbiology@gmail.com>
2025-02-27 11:38:53 -0800

    update pkg files

--- hmm/__init__.py
+++ hmm/__init__.py
@@ -8,4 +8,19 @@ Description:
 import numpy as np
 from .hmm import HiddenMarkovModel
 
-__version__ = '0.1.0'
+def _get_version():
+
+    import os
+
+    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
+
+    with open(pyproject_path, "r") as pyproject_file:
+        for line in pyproject_file.readlines():
+            if "version" in line:
+                return line.split("=")[1].strip().strip('"')
+
+
+try:
+    __version__ = _get_version()
+except Exception:
+    __version__ = "Unknown"
