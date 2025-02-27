40cf518944a73dbca8a042ccdcd5f14e9bdc94a0
Abe Arab <abarbiology@gmail.com>
2025-02-27 11:48:33 -0800

    implement tests

--- test/test_hmm.py
+++ test/test_hmm.py
@@ -3,11 +3,8 @@ from hmm import HiddenMarkovModel
 import numpy as np
 
 
-
-
 def test_mini_weather():
     """
-    TODO: 
     Create an instance of your HMM class using the "small_weather_hmm.npz" file. 
     Run the Forward and Viterbi algorithms on the observation sequence in the "small_weather_input_output.npz" file.
 
@@ -22,21 +19,30 @@ def test_mini_weather():
     mini_hmm=np.load('./data/mini_weather_hmm.npz')
     mini_input=np.load('./data/mini_weather_sequences.npz')
 
+    # instantiate HMM
+    hmm = HiddenMarkovModel(
+        observation_states=mini_hmm['observation_states'], 
+        hidden_states=mini_hmm['hidden_states'], 
+        prior_p=mini_hmm['prior_p'], 
+        transition_p=mini_hmm['transition_p'], 
+        emission_p=mini_hmm['emission_p']
+    )
 
+    # run forward algorithm
+    forward_prob = hmm.forward(mini_input['observation_state_sequence'])
+    # run viterbi algorithm
+    viterbi_path = hmm.viterbi(mini_input['observation_state_sequence'])
 
-
-
-
+    # check forward probability
+    assert np.isclose(forward_prob, 0.035064411621093736, atol=1e-6), "Forward probability does not match expected value"
     
-   
-    pass
-
+    # check for edge cases
+    assert len(viterbi_path) == len(mini_input['observation_state_sequence']), "Viterbi path length does not match input sequence length"
 
 
 def test_full_weather():
 
     """
-    TODO: 
     Create an instance of your HMM class using the "full_weather_hmm.npz" file. 
     Run the Forward and Viterbi algorithms on the observation sequence in the "full_weather_input_output.npz" file
         
@@ -45,17 +51,25 @@ def test_full_weather():
 
     """
 
-    pass
+    full_hmm=np.load('./data/full_weather_hmm.npz')
+    full_input=np.load('./data/full_weather_sequences.npz')
 
+    # instantiate HMM
+    hmm = HiddenMarkovModel(
+        observation_states=full_hmm['observation_states'], 
+        hidden_states=full_hmm['hidden_states'], 
+        prior_p=full_hmm['prior_p'], 
+        transition_p=full_hmm['transition_p'], 
+        emission_p=full_hmm['emission_p']
+    )
 
+    # run forward algorithm
+    forward_prob = hmm.forward(full_input['observation_state_sequence'])
+    # run viterbi algorithm
+    viterbi_path = hmm.viterbi(full_input['observation_state_sequence'])
 
+    # check forward probability
+    assert np.isclose(forward_prob, 1.6864513843961297e-11, atol=1e-6), "Forward probability does not match expected value"
 
-
-
-
-
-
-
-
-
-
+    # check for edge cases
+    assert len(viterbi_path) == len(full_input['observation_state_sequence']), "Viterbi path length does not match input sequence length"
